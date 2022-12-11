6caf37f4ff7f6cb4af25727e5cde27792dc81e8f
Sitong <sitongyang@sitongs-Air.lan>
2022-12-11 13:07:30 +1100

    fix graphics

--- snake_game.py
+++ snake_game.py
@@ -15,59 +15,6 @@ CELL_SIZE, CELL_NUMBER = 40, 20
 
 package_base_path = os.path.dirname(os.path.abspath(__file__))
 
-fruit_graphic = pygame.image.load(
-    os.path.join("./", "Assets", "Graphics", "apple.png")
-).convert_alpha()
-
-head_up_graphic = pygame.image.load(
-    os.path.join(package_base_path, "Assets", "Graphics", "head_up.png")
-).convert_alpha()
-head_down_graphic = pygame.image.load(
-    os.path.join(package_base_path, "Assets", "Graphics", "head_down.png")
-).convert_alpha()
-head_right_graphic = pygame.image.load(
-    os.path.join(package_base_path, "Assets", "Graphics", "head_right.png")
-).convert_alpha()
-head_left_graphic = pygame.image.load(
-    os.path.join(package_base_path, "Assets", "Graphics", "head_left.png")
-).convert_alpha()
-
-tail_up_graphic = pygame.image.load(
-    os.path.join(package_base_path, "Assets", "Graphics", "tail_up.png")
-).convert_alpha()
-tail_down_graphic = pygame.image.load(
-    os.path.join(package_base_path, "Assets", "Graphics", "tail_down.png")
-).convert_alpha()
-tail_right_graphic = pygame.image.load(
-    os.path.join(package_base_path, "Assets", "Graphics", "tail_right.png")
-).convert_alpha()
-tail_left_graphic = pygame.image.load(
-    os.path.join(package_base_path, "Assets", "Graphics", "tail_left.png")
-).convert_alpha()
-
-body_vertical_graphic = pygame.image.load(
-    os.path.join(package_base_path, "Assets", "Graphics", "body_vertical.png")
-).convert_alpha()
-body_horizontal_graphic = pygame.image.load(
-    os.path.join(package_base_path, "Assets", "Graphics", "body_horizontal.png")
-).convert_alpha()
-
-body_tr_graphic = pygame.image.load(
-    os.path.join(package_base_path, "Assets", "Graphics", "body_tr.png")
-).convert_alpha()
-body_tl_graphic = pygame.image.load(
-    os.path.join(package_base_path, "Assets", "Graphics", "body_tl.png")
-).convert_alpha()
-body_br_graphic = pygame.image.load(
-    os.path.join(package_base_path, "Assets", "Graphics", "body_br.png")
-).convert_alpha()
-body_bl_graphic = pygame.image.load(
-    os.path.join(package_base_path, "Assets", "Graphics", "body_bl.png")
-).convert_alpha()
-crunch_sound_graphic = pygame.mixer.Sound(
-    os.path.join(package_base_path, "Assets", "Sound", "crunch.wav")
-)
-
 
 class Fruit:
     def __init__(self):
@@ -163,6 +110,59 @@ snake_game = SnakeGame()
 
 canva = pygame.display.set_mode((CELL_SIZE * CELL_NUMBER, CELL_SIZE * CELL_NUMBER))
 clock = pygame.time.Clock()
+
+fruit_graphic = pygame.image.load(
+    os.path.join("./", "Assets", "Graphics", "apple.png")
+).convert_alpha()
+
+head_up_graphic = pygame.image.load(
+    os.path.join(package_base_path, "Assets", "Graphics", "head_up.png")
+).convert_alpha()
+head_down_graphic = pygame.image.load(
+    os.path.join(package_base_path, "Assets", "Graphics", "head_down.png")
+).convert_alpha()
+head_right_graphic = pygame.image.load(
+    os.path.join(package_base_path, "Assets", "Graphics", "head_right.png")
+).convert_alpha()
+head_left_graphic = pygame.image.load(
+    os.path.join(package_base_path, "Assets", "Graphics", "head_left.png")
+).convert_alpha()
+
+tail_up_graphic = pygame.image.load(
+    os.path.join(package_base_path, "Assets", "Graphics", "tail_up.png")
+).convert_alpha()
+tail_down_graphic = pygame.image.load(
+    os.path.join(package_base_path, "Assets", "Graphics", "tail_down.png")
+).convert_alpha()
+tail_right_graphic = pygame.image.load(
+    os.path.join(package_base_path, "Assets", "Graphics", "tail_right.png")
+).convert_alpha()
+tail_left_graphic = pygame.image.load(
+    os.path.join(package_base_path, "Assets", "Graphics", "tail_left.png")
+).convert_alpha()
+
+body_vertical_graphic = pygame.image.load(
+    os.path.join(package_base_path, "Assets", "Graphics", "body_vertical.png")
+).convert_alpha()
+body_horizontal_graphic = pygame.image.load(
+    os.path.join(package_base_path, "Assets", "Graphics", "body_horizontal.png")
+).convert_alpha()
+
+body_tr_graphic = pygame.image.load(
+    os.path.join(package_base_path, "Assets", "Graphics", "body_tr.png")
+).convert_alpha()
+body_tl_graphic = pygame.image.load(
+    os.path.join(package_base_path, "Assets", "Graphics", "body_tl.png")
+).convert_alpha()
+body_br_graphic = pygame.image.load(
+    os.path.join(package_base_path, "Assets", "Graphics", "body_br.png")
+).convert_alpha()
+body_bl_graphic = pygame.image.load(
+    os.path.join(package_base_path, "Assets", "Graphics", "body_bl.png")
+).convert_alpha()
+crunch_sound_graphic = pygame.mixer.Sound(
+    os.path.join(package_base_path, "Assets", "Sound", "crunch.wav")
+)
 
 while True:
     # 输入
